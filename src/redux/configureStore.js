66d7386b2705e5360c8a4f5f15beb5787673c336
MustafaYavas <mustafayavas40@gmail.com>
2023-03-25 20:14:36 +0300

    Update redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -18,11 +18,7 @@ const rootReducer = (state, action) => {
 
 const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-);
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(Saga);
 
