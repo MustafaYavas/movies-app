2bb5fc0d87c59c2964eb5ee927cfaba9e125d2ab
MustafaYavas <mustafayavas40@gmail.com>
2023-04-03 20:47:20 +0300

    Add text writer animation

--- src/components/Home/SectionOne.js
+++ src/components/Home/SectionOne.js
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination } from 'swiper';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import { TypeAnimation } from 'react-type-animation';
 
 import styles from './SectionOne.module.scss';
 import MovieCard from '../Card/MovieCard';
@@ -14,6 +15,7 @@ const SectionOne = ({ movies }) => {
   const [type, setType] = useState('cult');
   const [selectedMovies, setSelectedMovies] = useState([]);
   const [isPending, startTransition] = useTransition();
+  const [isSlideChange, setIsSlideChange] = useState(true);
 
   useEffect(() => {
     Aos.init({ duration: 1500 });
@@ -94,14 +96,27 @@ const SectionOne = ({ movies }) => {
           modifier: 1,
           slideShadows: true,
         }}
+        onSlideChangeTransitionStart={() => setIsSlideChange(false)}
+        onSlideChangeTransitionEnd={() => setIsSlideChange(true)}
         pagination={true}
         modules={[EffectCoverflow, Pagination]}
         className={`${styles['movie-tabs']}`}
       >
         {selectedMovies?.length > 0 &&
           selectedMovies?.map((movie) => (
-            <SwiperSlide key={movie.id}>
-              <MovieCard slide {...movie} />
+            <SwiperSlide key={movie}>
+              <div className="d-flex justify-content-center align-items-start">
+                <MovieCard slide {...movie} />
+
+                <p className="w-25 ms-5 mt-1 fst-italic">
+                  {isSlideChange && (
+                    <TypeAnimation
+                      sequence={[movie.overview, 10000]}
+                      speed={50}
+                    />
+                  )}
+                </p>
+              </div>
             </SwiperSlide>
           ))}
       </Swiper>
